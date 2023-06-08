2237c2bd0a557cb84c75dfd0738282ad00b3dbf8
Daniel Gelvez <danielgelvez.18@gmail.com>
2023-06-08 16:02:24 -0500

    finish integration tests

--- src/main/kotlin/com/training/xebia/functional/persistence/user/UserPersistence.kt
+++ src/main/kotlin/com/training/xebia/functional/persistence/user/UserPersistence.kt
@@ -1,8 +1,12 @@
 package com.training.xebia.functional.persistence.user
 
+import arrow.fx.coroutines.CircuitBreaker
+import arrow.fx.coroutines.Schedule
+import arrow.fx.coroutines.retry
 import com.training.xebia.functional.domain.SlackUserId
 import com.training.xebia.functional.domain.UserId
 import com.training.xebia.functional.domain.Users
+import kotlin.time.Duration.Companion.seconds
 
 interface UserPersistence {
     suspend fun findUsers(userIds: List<UserId>): List<Users>
@@ -18,8 +22,16 @@ fun UserPersistence(
 ): UserPersistence = UserPersistenceImpl(userQueries)
 
 private class UserPersistenceImpl(private val userQueries: UsersQueries) : UserPersistence {
+
+
+    suspend fun <A> resilient(schedule: Schedule<Throwable, *>, f: suspend () -> A): A =
+        schedule.retry { setCircuitBreaker().protectOrThrow(f) }
+
     override suspend fun findUsers(userIds: List<UserId>): List<Users> =
-        userQueries.findUsers(userIds, ::Users).executeAsList()
+        Schedule.recurs<Throwable>(5).retry {
+            setCircuitBreaker().protectOrThrow(userQueries.findUsers(userIds).executeAsList())
+        }
+
 
     override suspend fun find(userId: UserId): Users? =
         userQueries.find(userId, ::Users).executeAsOneOrNull()
@@ -41,4 +53,13 @@ private class UserPersistenceImpl(private val userQueries: UsersQueries) : UserP
 
     override suspend fun deleteUser(userId: UserId): Unit =
         userQueries.deleteUser(userId)
+}
+
+suspend fun setCircuitBreaker(): CircuitBreaker {
+    return CircuitBreaker.of(
+        maxFailures = 2,
+        resetTimeout = 2.seconds,
+        exponentialBackoffFactor = 2.0, // enable exponentialBackoffFactor
+        maxResetTimeout = 60.seconds, // limit exponential back-off time
+    )
 }
